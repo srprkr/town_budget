1490c8895616b7bb97c13d06a898e33cafe37b68
Sean Parker <seanrichardparker@gmail.com>
2026-05-13 16:34:52 -0400

    add source link

--- src/components/BudgetChart.jsx
+++ src/components/BudgetChart.jsx
@@ -295,6 +295,8 @@ const BudgetChart = ({ data, title, drillDownData = {} }) => {
           {breadcrumb.length === 0
             ? 'Click a fund to drill down · Scroll to zoom · Double-click to reset zoom'
             : `Viewing: ${breadcrumb[breadcrumb.length - 1]} · Click center to return`}
+            <br />
+            <a target="_blank" href="https://jenkintownboro.com/budgets/">Source</a>
         </div>
         {tooltip && (
           <div
