84230ef7549220b7751ef3f0155a1aeef4b9ac5f
Sean Parker <seanrichardparker@gmail.com>
2026-05-14 19:05:21 -0400

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/town_budget/',
+  "deploy": "gh-pages -d dist/town_budget"
+
   plugins: [react()],
 })
